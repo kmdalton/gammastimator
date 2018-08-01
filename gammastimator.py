b57a7314d30912b0914e2b6e35b7f1d77a435be2
Kevin Dalton <kevinmdalton@gmail.com>
2018-08-01 09:31:11 -0400

    new func to extract image metadata 	modified:   gammastimator.py

--- gammastimator.py
+++ gammastimator.py
@@ -3,6 +3,14 @@ import numpy as np
 import argparse
 
 
+def image_metadata(dataframe, keys = None):
+    if keys is None:
+        keys = [k for k in dataframe.keys() if 'ipm' in k.lower()]
+        specifically_check = ['Io', 'BEAMX', 'BEAMY', 'Icryst']
+        for k in specifically_check:
+            if k in dataframe:
+                keys.append(k)
+    return dataframe[['RUN', 'IMAGENUMBER'] + list(keys)].groupby(['RUN','IMAGENUMBER']).mean()
 
 
 def main():
